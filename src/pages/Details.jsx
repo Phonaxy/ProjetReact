5d5a62976abb25b4ca6994ac652932dd44ed7c51
Phonaxy <romaindestruhaut@yahoo.fr>
2025-04-09 14:58:41 +0200

    add pages+nav

---
+++ src/pages/Details.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+
+const API_KEY = "7c51ce5f51b2a0e2bb3bf45b2afaa9ae";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+
+function Details() {
+    return (
+        <div>
+            <h1>Details</h1>
+        </div>
+    );
+}
+
+export default Details;
---
+++ src/pages/News.jsx
@@ -0,0 +1,3 @@
+export default function News() {
+    return <h1>Page trouvée</h1>;
+}
