2c62412298250a502093c7d457a1bdeee9db9b8c
Phonaxy <romaindestruhaut@yahoo.fr>
2025-04-16 17:26:10 +0200

    improvement Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import homeVideo from '../assets/home.mp4';
+import '../css/style.css';
 
 function Home() {
     return (
-        <div>
+        <div className="home-container">
             <Header />
-            <h1>Home</h1>
+            <main className="main-content">
+                <div className="video-background">
+                    <video autoPlay loop muted playsInline className="video-bg">
+                        <source src={homeVideo} type="video/mp4"/>
+                        Votre navigateur ne supporte pas la vidéo HTML5.
+                    </video>
+                    <div className="video-overlay"></div>
+                </div>
+
+                <div className="video-overlay-content">
+                    <h1>Bienvenue</h1>
+                    <p>Explore notre univers</p>
+                </div>
+            </main>
+
             <Footer />
         </div>
     );
