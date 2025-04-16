444f1093cac02cf2abc38b262c0cfd3dac9273e8
Fasjupy <beguehouett@gmail.com>
2025-04-16 15:03:46 +0200

    add Header + fix structure

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const API_KEY = "7c51ce5f51b2a0e2bb3bf45b2afaa9ae";
 const BASE_URL = "https://api.themoviedb.org/3";
@@ -7,7 +9,9 @@ const BASE_URL = "https://api.themoviedb.org/3";
 function Details() {
     return (
         <div>
+            <Header/>
             <h1>Details</h1>
+            <Footer/>
         </div>
     );
 }
